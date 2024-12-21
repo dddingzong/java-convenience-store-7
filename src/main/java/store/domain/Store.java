bdae285b99f5eacde7c43ae07e284339940ccc73
USER <cji513@naver.com>
2024-12-21 17:06:09 +0900

    feat(Store): Store 도메인 생성

---
+++ src/main/java/store/domain/Store.java
@@ -0,0 +1,18 @@
+package store.domain;
+
+import java.util.List;
+
+public class Store {
+
+    private List<Product> products;
+    private List<Promotion> promotions;
+
+    public void registerProduct(){
+        this.products = products;
+    }
+
+    public void registerPromotion(){
+        this.promotions = promotions;
+    }
+
+}
